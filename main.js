5285f07a9971a943905744685171cbc2f641bfa3
AnshulTheGreat <69811258+AnshulTheGreat@users.noreply.github.com>
2021-06-11 16:37:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function function1(){
+   window.location = "object1.html";
+}
+function function2(){
+    window.location = "object2.html";
+ }
+
+ function function3(){
+    window.location = "object3.html";
+ }
+ function function4(){
+    window.location = "object4.html";
+ }
+ function function5(){
+    window.location = "object5.html";
+ }
+ function function6(){
+    window.location = "object6.html";
+ }
